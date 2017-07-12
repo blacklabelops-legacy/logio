d0b2109d7bba15a339f804c48f6a27e7376d8443
Richard Bateman <taxilian@gmail.com>
2017-07-12 14:46:22 -0600

    Change harvester run method to use node module and auto-detect containers with names

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,28 @@
-FROM blacklabelops/centos
-MAINTAINER Steffen Bleul <sbl@blacklabelops.com>
+FROM node:6
+MAINTAINER Richard Bateman <taxilian@gmail.com>
 
 # Propert permissions
 ENV CONTAINER_USER logio
-ENV CONTAINER_UID 1000
+ENV CONTAINER_UID 2000
 ENV CONTAINER_GROUP logio
-ENV CONTAINER_GID 1000
+ENV CONTAINER_GID 2000
 
 RUN /usr/sbin/groupadd --gid $CONTAINER_GID logio && \
     /usr/sbin/useradd --uid $CONTAINER_UID --gid $CONTAINER_GID --create-home --shell /bin/bash logio
 
 # install dev tools
 ENV VOLUME_DIRECTORY=/opt/server
-RUN curl --silent --location https://rpm.nodesource.com/setup | bash - && \
-    yum install -y \
-    curl \
-    nodejs \
-    wget \
-    gcc-c++ \
-    openssl \
-    make && \
-    yum clean all && rm -rf /var/cache/yum/* && \
-    mkdir -p ${VOLUME_DIRECTORY}/keys && \
-    chown -R $CONTAINER_UID:$CONTAINER_GID ${VOLUME_DIRECTORY}/keys && \
-    npm install -g log.io --user 'root'
+RUN DEBIAN_FRONTEND=noninteractive apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y wget openssl build-essential \
+    && mkdir -p ${VOLUME_DIRECTORY}/keys \
+    && chown -R $CONTAINER_UID:$CONTAINER_GID ${VOLUME_DIRECTORY}/keys \
+    && npm install -g log.io pm2 --user 'root' \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists \
+    && rm -rf /tmp/*
+
+COPY configGen /opt/configGen
+RUN cd /opt/configGen && npm i
 
 ENV DELAYED_START=
 ENV LOGIO_ADMIN_USER=
@@ -42,5 +41,6 @@ EXPOSE 28778 28777
 
 USER $CONTAINER_UID
 COPY imagescripts/*.sh /opt/logio/
+
 ENTRYPOINT ["/opt/logio/docker-entrypoint.sh"]
 CMD ["logio"]
---
+++ configGen/run_harvester.js
@@ -0,0 +1,139 @@
+// jshint esversion:6
+
+// First check the config
+
+const fs = require('fs');
+const Docker = require('node-docker-api').Docker;
+const DockerEvents = require('docker-events');
+const Dockerode = require('dockerode');
+const which = require('which');
+const { execFile } = require('child_process');
+const _ = require('lodash');
+
+const harvesterPathDfd = new Promise((resolve, reject) => {
+    which('log.io-harvester', (err, path) => err ? reject(err) : resolve(path));
+});
+
+const requireExplicit = process.env.LOGIO_REQUIRE_EXPLICIT == 1;
+const logio_nodeName = process.env.LOGIO_HARVESTER_NODENAME || "node";
+const logio_streamPrefix = process.env.LOGIO_HARVESTER_PREFIX || null;
+const logio_master = process.env.LOGIO_HARVESTER_MASTER_HOST || "logio";
+const logio_masterPort = process.env.LOGIO_HARVESTER_MASTER_HOST || "28777";
+
+const docker_sock = process.env.DOCKER_SOCKET || "/tmp/docker.sock";
+
+const configFilePath = process.env.LOGIO_HARVESTER_CONFIG || "/root/.log.io/harvester.conf"
+
+const dockerode = new Dockerode({socketPath: docker_sock})
+const docker = new Docker({ socketPath: docker_sock });
+
+const dockerEmitter = new DockerEvents({docker: dockerode});
+
+function parseEnv(env) {
+    let out = {};
+    for (let e of env) {
+        let [k, val] = e.split('=', 2);
+        out[k] = val;
+    }
+    return out;
+}
+
+function refreshConfig() {
+
+    // Detect all containers and get their status
+    let containers = docker.container.list().then(list => Promise.all(list.map(c => c.status())));
+
+    // Loop through and collect config information
+    return containers.then(list => {
+        const config = {};
+        for (let c of list) {
+            const cenv = parseEnv(c.data.Config.Env);
+            // if requireExplicit is set then we only include containers with
+            // LOGIO_INCLUDE=1 set in the environment
+            if (requireExplicit && cenv.LOGIO_INCLUDE != 1 || cenv.LOGIO_EXCLUDE == 1) {
+                continue;
+            }
+            // console.log(`env for ${c.data.Name}:`, cenv);
+            let name = cenv.LOGIO_STREAM || c.data.Name.replace(/^\//g, '');
+            if (logio_streamPrefix) { name = `${logio_streamPrefix}${name}`; }
+            let logPath = c.data.LogPath;
+
+            config[name] = {
+                name: name,
+                path: logPath
+            };
+        }
+        return config;
+    }).then(generateConfigFile);
+}
+
+function generateConfigFile(config) {
+    let configFile = `
+exports.config = {
+    nodeName: ${JSON.stringify(logio_nodeName)},
+    logStreams: {`;
+    for (let stream of Object.keys(config)) {
+        configFile += `
+        ${JSON.stringify(stream)}: [${JSON.stringify(config[stream].path)}],`;
+    }
+
+    configFile += `
+    },
+    server: {
+        host: ${JSON.stringify(logio_master)},
+        port: ${logio_masterPort}
+    }
+}
+`;
+    return configFile;
+}
+
+function updateConfig(msg) {
+    if (msg) {
+        console.warn("Message from docker:", msg);
+    }
+    return refreshConfig().then(fileContents => {
+        console.log("Writing new harvester config!", fileContents);
+        fs.writeFileSync(configFilePath, fileContents);
+        return startHarvester();
+    }).catch(err => {
+        console.warn("Error updating config file!", err);
+    });
+}
+updateConfig = _.debounce(updateConfig, 500);
+
+let harvester;
+let harvestPath;
+function startHarvester() {
+    if (harvester) {
+        harvester.kill();
+        harvester = void 0;
+        return;
+    }
+    harvester = execFile(harvestPath, (err, stdout, stderr) => {
+        console.log("Harvester terminated:", err, stderr);
+        setImmediate(startHarvester);
+    });
+    console.log("Re-spawned harvester");
+}
+
+function initHarvester() {
+    harvesterPathDfd.then(hp => {
+        console.log("Found harvester at", hp);
+        harvestPath = hp;
+        return updateConfig();
+    }).catch(err => {
+        console.warn("Could not initalize harvester!", err);
+    });
+}
+
+dockerEmitter.on("connect", () => console.log("Connected to docker, waiting for events"));
+
+dockerEmitter.on("start", updateConfig);
+dockerEmitter.on("stop", updateConfig);
+dockerEmitter.on("die", updateConfig);
+// dockerEmitter.on("_message", message => console.log("got a message from docker: %j", message));
+
+dockerEmitter.start();
+
+initHarvester();
